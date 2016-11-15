ea78545c4353626b76e712ff521ccd8328a497f3
wsickert <wesley.sickert@gmail.com>
2016-11-15 20:46:23 -0200

    create session and login

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,5 @@
 module PagesHelper
+  def upload
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 
   get '/signup-user' => 'users#new'
   post '/users' => 'users#create'
+
+  get    'sign_in'   => 'sessions#new'
+  post   'sign_in'   => 'sessions#create'
+  delete 'sign_out'  => 'sessions#destroy'
+
+  get '/pages/upload' => 'pages#upload'
 end
