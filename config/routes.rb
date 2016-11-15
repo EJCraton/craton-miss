4c07deb2fa78ee1821cf43a7bccbe76dd4a23b14
wsickert <wesley.sickert@gmail.com>
2016-11-15 17:18:26 -0200

    create users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
+
+  get '/signup-user' => 'users#new'
+  post '/users' => 'users#create'
 end
