1c13b80a7b310e66edd94372e691e6c63ac367e8
Victor Arnaud <victorhad@gmail.com>
2016-11-18 14:17:24 -0200

    Layout do CRUD de imagens finalizado

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -1,4 +1,4 @@
 class Archive < ApplicationRecord
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>"  }
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "200x200>"  }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
